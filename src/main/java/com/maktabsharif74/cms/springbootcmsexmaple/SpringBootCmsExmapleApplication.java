a4b0501e19155370ad111687f539d964a0cf96c1
maxixking66 <maxixking66@gamil.com>
2022-10-06 13:03:29 +0330

    add another properties file and use it with @PropertySource

--- src/main/java/com/maktabsharif74/cms/springbootcmsexmaple/SpringBootCmsExmapleApplication.java
+++ src/main/java/com/maktabsharif74/cms/springbootcmsexmaple/SpringBootCmsExmapleApplication.java
@@ -2,8 +2,10 @@ package com.maktabsharif74.cms.springbootcmsexmaple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:cookie.properties")
 public class SpringBootCmsExmapleApplication {
 
     public static void main(String[] args) {
