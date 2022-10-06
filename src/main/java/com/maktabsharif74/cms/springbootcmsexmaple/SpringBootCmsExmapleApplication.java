ce20ca6b68935e4d36b28a6e4ebd86673a191932
maxixking66 <maxixking66@gamil.com>
2022-10-06 09:01:44 +0330

    init

---
+++ src/main/java/com/maktabsharif74/cms/springbootcmsexmaple/SpringBootCmsExmapleApplication.java
@@ -0,0 +1,13 @@
+package com.maktabsharif74.cms.springbootcmsexmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCmsExmapleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCmsExmapleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maktabsharif74/cms/springbootcmsexmaple/SpringBootCmsExmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maktabsharif74.cms.springbootcmsexmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCmsExmapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
